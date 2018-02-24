118a19a2ba9c57b23e8e3723e810335a262a5824
AS SH <sushmaawaghmare@gmail.com>
2018-02-25 00:41:00 +0530

    new build files for rxjava

--- domain/src/main/java/org/talcrafts/udhari/domain/repository/TxnRepository.java
+++ domain/src/main/java/org/talcrafts/udhari/domain/repository/TxnRepository.java
@@ -4,12 +4,14 @@ import org.talcrafts.udhari.domain.TransactionEntity;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+
 /**
  * Created by sushma on 24/2/18.
  */
 
 public interface TxnRepository {
-    public List<TransactionEntity> txns();
+    public Observable<List<TransactionEntity>> txns();
 
-    public TransactionEntity txn();
+    public Observable<TransactionEntity> txn();
 }
